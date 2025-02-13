cd46a7fab11a56322644270778e925ab01bae490
coactis1 <c.actis@live.com>
2025-02-13 19:38:30 +0100

    first-commit

---
+++ components/sections/Hero1.tsx
@@ -0,0 +1,21 @@
+
+
+export default function Hero1() {
+	return (
+		<>
+
+			<section className="box-section block-banner-home1 position-relative">
+				<div className="container position-relative z-1">
+					<p className="text-primary text-md-bold wow fadeInUp">Encuentra tu coche perfecto</p>
+					<h1 className="color-white mb-35 wow fadeInUp">¿Buscas un vehículo? <br className="d-none d-lg-block" />Estás en el lugar perfecto.</h1>
+					<ul className="list-ticks-green">
+						<li className="wow fadeInUp" data-wow-delay="0.1s">Alta calidad</li>
+						<li className="wow fadeInUp" data-wow-delay="0.2s">Servicio premium</li>
+						<li className="wow fadeInUp" data-wow-delay="0.4s">Garantía de satisfacción</li>
+					</ul>
+				</div>
+				<div className="bg-shape z-0" />
+			</section>
+		</>
+	)
+}
